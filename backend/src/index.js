50831d71596579723131745760c2c3a09846431c
Tomás Lopes <tomas050302@gmail.com>
2020-02-10 21:46:49 +0000

    feat(mongoose): mongoose Config and Controller setup

---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const mongooseUrl = require('./config/mongooseSettings');
+
+const app = express();
+
+mongoose.connect(mongooseUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
---
+++ backend/src/routes.js
@@ -0,0 +1,5 @@
+const { Router } = require('express');
+
+const routes = Router();
+
+module.exports = routes;
