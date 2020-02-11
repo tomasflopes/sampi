787e0634d92412f635373d56392c2ac81296222e
Tomás Lopes <tomas050302@gmail.com>
2020-02-11 23:10:31 +0000

    feat(playercontroller): delete function and add name duplication check
    Delete function is now working and the program can check if there are any existing users with the
    same name as the one who is trying to register.

--- backend/src/controllers/PlayerController.js
+++ backend/src/controllers/PlayerController.js
@@ -1,4 +1,4 @@
-const Player = require('../models/Player')
+const Player = require('../models/Player');
 
 module.exports = {
   async index(request, response) {
@@ -10,28 +10,45 @@ module.exports = {
   async store(request, response) {
     const { name, avatar_url, sex, birth, email, phone, position } = request.body;
 
-    player = await Player.create({
-      name,
-      avatar_url,
-      sex,
-      birth,
-      email,
-      phone,
-      position
-    });
+    let player = await Player.findOne({ name });
 
-    return response.json(`${name} was registred!`);
+    if (!player) {
+      player = await Player.create({
+        name,
+        avatar_url,
+        sex,
+        birth,
+        email,
+        phone,
+        position
+      });
+    }
+
+    return response.json(player);
   },
 
   async update(request, response) {
+    const { name } = request.query;
 
+    const updateInfo = await Player.update(
+      { name },
+      {
+        $set: {
+          //TODO Loop trough the request.body object and update every field
+        }
+      },
+    );
+
+    return response.json(updateInfo);
   },
 
   async destroy(request, response) {
+    const { name } = request.query;
 
-  },
-};
+    const deletedPlayer = await Player.deleteOne({
+      name,
+    });
 
-/* function existsUserWithThisName(name) {
-  return await Player.findOne({ name });
-} */
+    return response.json(deletedPlayer);
+  }
+}
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,8 +4,8 @@ const PlayerController = require('./controllers/PlayerController');
 const routes = Router();
 
 routes.get('/player', PlayerController.index);
-routes.post('/player', PlayerController.store);/*
-routes.put('/player', PlayerController.update);
-routes.delete('/player', PlayerController.destroy); */
+routes.post('/player', PlayerController.store);
+/* routes.put('/player', PlayerController.update); */
+routes.delete('/player', PlayerController.destroy);
 
 module.exports = routes;
