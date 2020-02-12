4b33956d0849d7ecde6c3c12440e01aea799c340
Tomás Lopes <tomas050302@gmail.com>
2020-02-12 11:19:22 +0000

    feat(mongoosesetting): configurated new db on mongoose for testing

--- backend/src/config/mongooseSettings.js
+++ backend/src/config/mongooseSettings.js
@@ -1,1 +1,4 @@
-module.exports = 'mongodb+srv://kappa:kappa@sampi-je1rj.mongodb.net/test?retryWrites=true&w=majority';
+module.exports = {
+  production: 'mongodb+srv://kappa:kappa@sampi-je1rj.mongodb.net/sampi?retryWrites=true&w=majority',
+  test: 'mongodb+srv://kappa:kappa@sampi-je1rj.mongodb.net/test?retryWrites=true&w=majority'
+}
