bb43cd840c5ccb9499e3806a4a1ba8938e1fae56
tomas050302 <tomas050302@gmail.com>
2020-02-13 21:44:17 +0000

    feat(routes): started basic routing

---
+++ mobile/src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function Home() {
+  return (
+    <View>
+      <Text>Lala</Text>
+    </View>
+  )
+}
+
---
+++ mobile/src/routes.js
@@ -0,0 +1,8 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createStackNavigator();
+
+export default function Routes() {
+
+};
