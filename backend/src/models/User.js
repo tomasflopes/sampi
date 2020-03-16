c835c941eaebf67bf78fa17c2170d310d2576be4
Tomás Lopes <tomas050302@gmail.com>
2020-03-16 02:20:18 +0000

    chore(faker): added faker dependency

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
   email: String,
-  password: String,
+  password_hash: String,
 });
 
 module.exports = mongoose.model('User', UserSchema);
