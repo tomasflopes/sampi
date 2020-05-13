3bd8559a277f2e015ac09fba864fff3828b85e50
Tomás Lopes <tomas050302@gmail.com>
2020-05-14 00:43:16 +0100

    feat(group managing): added all the group logic to home screen

--- mobile/src/contexts/auth.js
+++ mobile/src/contexts/auth.js
@@ -5,17 +5,26 @@ const AuthContext = createContext({ signed: false });
 
 export const AuthProvider = ({ children }) => {
   const [signed, setSigned] = useState(false);
+  const [hasGroup, setHasGroup] = useState(false);
 
   function SignIn() {
     setSigned(true);
   }
 
-  function signOut() {
+  function SignOut() {
     setSigned(false);
   }
 
+  function UserHasGroup() {
+    setHasGroup(true);
+  }
+
+  function UserDoesNotHaveGroup() {
+    setHasGroup(false);
+  }
+
   return (
-    <AuthContext.Provider value={{ signed, SignIn, signOut }}>
+    <AuthContext.Provider value={{ signed, SignIn, SignOut, hasGroup, UserHasGroup, UserDoesNotHaveGroup }}>
       {children}
     </AuthContext.Provider>
   );
