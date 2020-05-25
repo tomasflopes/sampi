6626804319a85ada1fe85025ed254deb5b21b19e
Tomás Lopes <tomas050302@gmail.com>
2020-05-25 20:41:22 +0100

    feat(finish game screen): styled half of the screen

--- mobile/src/screens/FinishGame/index.js
+++ mobile/src/screens/FinishGame/index.js
@@ -1,12 +1,70 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Modal, TouchableOpacity, Image } from 'react-native';
+
+import styles from './styles';
+
+import TopBar from '../../components/TopBar';
 
 import api from '../../services/api';
 
-export default function FinishGame() {
+export default function FinishGame({ navigation }) {
+  const [isVisible, setIsVisible] = useState(false);
+
+  function toggleIsVisible() {
+    setIsVisible(!isVisible);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Finish Game</Text>
+      <TopBar navigation={navigation} />
+      <Modal visible={isVisible}>
+
+      </Modal>
+
+      <Text style={styles.headerText}>So bla bla</Text>
+
+      <View style={styles.resultContainer}>
+        <View style={styles.teamContainer}>
+          <View style={styles.teamNameContainer}>
+            <Text style={styles.teamNameText}>Team A</Text>
+          </View>
+          <View style={styles.teamResultContainer}>
+            <Text style={styles.teamResultText}>0</Text>
+          </View>
+        </View>
+
+        <Text style={styles.resultDivider}>x</Text>
+
+        <View style={styles.teamContainer}>
+          <View style={styles.teamNameContainer}>
+            <Text style={styles.teamNameText}>Team B</Text>
+          </View>
+          <View style={styles.teamResultContainer}>
+            <Text style={styles.teamResultText}>0</Text>
+          </View>
+        </View>
+      </View>
+
+      <Text style={styles.hintText}>Hint</Text>
+
+      <View style={styles.mvpPickerContainer}>
+        <TouchableOpacity style={styles.mvpPicker}>
+          <Image
+            style={styles.mvpStart}
+            source={require('../../../assets/mvpStar.png')}
+          />
+          <Text style={styles.mvpPickerText}>Pick MVP</Text>
+          <Image
+            style={styles.mvpStart}
+            source={require('../../../assets/mvpStar.png')}
+          />
+        </TouchableOpacity>
+        <Text>Note</Text>
+      </View>
+
+      <TouchableOpacity>
+        <Text>Confirm</Text>
+      </TouchableOpacity>
     </View>
   );
 }
--- mobile/src/screens/FinishGame/styles.js
+++ mobile/src/screens/FinishGame/styles.js
@@ -4,4 +4,90 @@ import { general, colors } from '../../styles'
 
 export default styles = StyleSheet.create({
   container: general.container,
+
+  headerText: {
+    flex: 2,
+    fontSize: 18,
+    color: colors.mainBlue,
+    paddingHorizontal: 10,
+    textAlign: 'center',
+    marginTop: 10,
+  },
+
+  resultContainer: {
+    flex: 4,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+
+  teamContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  teamNameContainer: {
+    flex: 1,
+    textAlign: 'center',
+  },
+
+  teamNameText: {
+    flex: 1,
+    fontSize: 15,
+  },
+
+  teamResultContainer: {
+    width: 55,
+    height: 55,
+    borderWidth: 1,
+    borderColor: colors.mainBlue,
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  teamResultText: {
+    fontSize: 25,
+    flex: 1,
+    textAlign: 'center',
+    color: colors.lightGray,
+  },
+
+  resultDivider: {
+    flex: 2,
+    textAlign: 'center',
+    fontSize: 25,
+  },
+
+  hintText: {
+    fontSize: 13,
+    paddingHorizontal: 5,
+    color: colors.midGray,
+    flex: 1,
+    textAlign: 'center',
+    marginTop: 5,
+  },
+
+  mvpPickerContainer: {
+    flex: 4,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  mvpPicker: {
+    flex: 3,
+    width: 180,
+    borderWidth: 1,
+    borderColor: colors.mainBlue,
+    borderRadius: 5,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  mvpPickerText: {
+    fontSize: 18,
+    color: colors.activeDarkBlue,
+  }
 });
